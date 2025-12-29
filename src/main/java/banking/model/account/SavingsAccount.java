9fb25c84bbf5794ec0838139174969b1a0e98234
HP <ismailkarali661@gmail.com>
2025-12-29 20:39:57 +0300

    Add SavingsAccount with overridden withdraw method

---
+++ src/main/java/banking/model/account/SavingsAccount.java
@@ -0,0 +1,18 @@
+package banking.model.account;
+
+/**
+ * Savings account with no overdraft allowed.
+ */
+public class SavingsAccount extends Account {
+
+    public SavingsAccount(String accountNumber, double balance) {
+        super(accountNumber, balance);
+    }
+
+    @Override
+    public void withdraw(double amount) {
+        if (amount > 0 && balance >= amount) {
+            balance -= amount;
+        }
+    }
+}
